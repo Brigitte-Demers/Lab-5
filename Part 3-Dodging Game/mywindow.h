19b251665ecbe5c6b5cb3ec7ddd8b328c5500f14
Brigitte-Demers <55590862+Brigitte-Demers@users.noreply.github.com>
2020-02-23 17:48:21 -0500

    Add files via upload

---
+++ Part 3-Dodging Game/mywindow.h
@@ -0,0 +1,10 @@
+#ifndef MYWINDOW_H
+#define MYWINDOW_H
+
+#define WINDOW_WIDTH    800
+#define WINDOW_HEIGHT   600
+#define CONSOLE_START_Y 240 // Adds the CONSOLE_START_Y to the header file.
+const int window_w = WINDOW_WIDTH;
+const int window_h = WINDOW_HEIGHT;
+
+#endif
